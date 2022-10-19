a40a573efdd8f4bb6a6c239b58e11a223f9b20e5
Jo YoungHan <verin4494@gmail.com>
2022-10-19 21:15:57 +0900

    file attach

---
+++ upload/src/main/java/hello/upload/domain/Item.java
@@ -0,0 +1,21 @@
+package hello.upload.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+
+}
