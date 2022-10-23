03a3e81cbf099cd1a666ce47df0098519a255efa
Jo YoungHan <verin4494@gmail.com>
2022-10-23 19:45:17 +0900

    thymeleaf1

--- thymeleaf-basic-prac/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic-prac/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -128,4 +128,19 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+
+
 }
